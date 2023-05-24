d10d13ade596feb27ea60fcea5309f95718b8f26
Ivan Ermolaev <ivev152@gmail.com>
2023-05-25 02:29:05 +0400

    tested stored functions from "customer" group

---
+++ src/ComputerEquipmentMS/Server.Test/CustomerFunctionsTests/GetCustomerByContact.cs
@@ -0,0 +1,42 @@
+﻿using Server.Models.Auxiliary;
+
+namespace Server.Test.CustomerFunctionsTests;
+
+public class GetCustomerByContact : CustomerFunctionsTestBase
+{
+    [Theory]
+    [MemberData(nameof(ExistingContacts))]
+    public void GetCustomersByExistingContactShouldReturnCorrectCustomerIds(
+        ContactType contactType, 
+        string contact, 
+        ICollection<int> expectedCustomerIds) 
+    {
+        var customers = Executor.GetCustomersByContact(contactType, contact);
+        var customerIds = customers.Select(customer => customer.Id); 
+        customerIds.Should().BeEquivalentTo(expectedCustomerIds);
+    }
+    
+    public static IEnumerable<object[]> ExistingContacts =>
+        new List<object[]>
+        {
+            new object[] { ContactType.Email, "annagmail@gmail.com", new [] {2} },
+            new object[] { ContactType.Phone, "8-800-555-35-35", new [] {3} },
+        };
+    
+    
+    
+    [Theory]
+    [MemberData(nameof(NonExistingContacts))]
+    public void GetCustomersByNotExistingContactShouldReturnEmptyCollection(ContactType contactType, string contact) 
+    {
+        var customers = Executor.GetCustomersByContact(contactType, contact);
+        customers.Should().BeEmpty();
+    }
+    
+    public static IEnumerable<object[]> NonExistingContacts =>
+        new List<object[]>
+        {
+            new object[] { ContactType.Email, "email" },
+            new object[] { ContactType.Phone, "phone" },
+        };
+}
---
+++ src/ComputerEquipmentMS/Server.Test/CustomerFunctionsTests/GetCustomerPurchasesForTimeInterval.cs
@@ -0,0 +1,104 @@
+﻿using NodaTime;
+using Server.Models.Auxiliary;
+
+namespace Server.Test.CustomerFunctionsTests;
+
+public class GetCustomerPurchasesForTimeInterval : CustomerFunctionsTestBase
+{
+    [Theory]
+    [MemberData(nameof(ExistingCustomerIdsWithPurchases))]
+    public void GetPurchasesOfExistingCustomerWithPurchasesForTimeIntervalShouldReturnCorrectSalesIds(
+        int customerId, 
+        TimeInterval timeInterval,
+        ICollection<int> expectedSaleIds) 
+    {
+        var sales = Executor.GetCustomersPurchases(customerId, timeInterval);
+        var saleIds = sales.Select(customer => customer.Id); 
+        saleIds.Should().BeEquivalentTo(expectedSaleIds);
+    }
+    
+    public static IEnumerable<object[]> ExistingCustomerIdsWithPurchases =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                1,
+                new TimeInterval(
+                    Instant.FromUtc(2021, 05, 30, 12, 00),
+                    Instant.FromUtc(2022, 05, 30, 12, 00)),
+                new [] {1, 3}
+            },
+            new object[]
+            {
+                1,
+                new TimeInterval(
+                    Instant.FromUtc(2022, 04, 30, 12, 00),
+                    Instant.FromUtc(2022, 06, 30, 12, 00)),
+                new [] {3}
+            },
+            new object[]
+            {
+                2,
+                new TimeInterval(
+                    Instant.FromUtc(2019, 10, 10, 12, 00),
+                    Instant.FromUtc(2022, 11, 30, 12, 00)),
+                new [] {2}
+            },
+            new object[]
+            {
+                2,
+                new TimeInterval(
+                    Instant.FromUtc(1999, 10, 10, 12, 00),
+                    Instant.FromUtc(2010, 11, 30, 12, 00)),
+                Array.Empty<int>()
+            },
+        };
+
+    [Theory]
+    [MemberData(nameof(ExistingCustomerIdsWithNoPurchases))]
+    public void GetPurchasesOfExistingCustomerWithNoPurchasesShouldReturnEmptyCollection(
+        int customerId, 
+        TimeInterval timeInterval) 
+    {
+        var sales = Executor.GetCustomersPurchases(customerId, timeInterval);
+        sales.Should().BeEmpty();
+    }
+    
+    public static IEnumerable<object[]> ExistingCustomerIdsWithNoPurchases =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                3,
+                new TimeInterval(
+                    Instant.FromUtc(2022, 04, 30, 12, 00),
+                    Instant.FromUtc(2022, 06, 30, 12, 00)),
+            },
+        };
+    
+    [Theory]
+    [MemberData(nameof(NonExistingCustomerIds))]
+    public void GetPurchasesOfNonExistingCustomerShouldReturnEmptyCollection(int customerId, TimeInterval timeInterval) 
+    {
+        var sales = Executor.GetCustomersPurchases(customerId);
+        sales.Should().BeEmpty();
+    }
+    
+    public static IEnumerable<object[]> NonExistingCustomerIds =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                45,
+                new TimeInterval(
+                    Instant.FromUtc(2022, 04, 30, 12, 00),
+                    Instant.FromUtc(2022, 06, 30, 12, 00)),
+            },
+            new object[] {
+                54,
+                new TimeInterval(
+                    Instant.FromUtc(2022, 04, 30, 12, 00),
+                    Instant.FromUtc(2022, 06, 30, 12, 00)),
+            },
+        };
+}
