2b6afe033656fa96265425eda6e91a6bb4d69b62
Ivan Ermolaev <ivev152@gmail.com>
2023-06-10 17:01:29 +0400

    clean Program.cs

---
+++ src/ComputerEquipmentMS/ComputerEquipmentMS/ApplicationServiceConfigurators/AuthConfigurator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace ComputerEquipmentMS.ApplicationServiceConfigurators;
+
+public static class AuthConfigurator
+{
+    public static void AddAuth(this IServiceCollection services)
+    {
+        services.AddAuthorization(
+            options => options.AddPolicy(
+                "defaultPolicy",
+                policy => policy.RequireAuthenticatedUser()
+            )
+        );
+        
+        services
+            .AddAuthentication(options => options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options => options.LoginPath = "/Auth/Login");
+    }
+}
--- src/ComputerEquipmentMS/ComputerEquipmentMS/Program.cs
+++ src/ComputerEquipmentMS/ComputerEquipmentMS/Program.cs
@@ -1,26 +1,24 @@
 using System.Globalization;
-using System.Reflection;
 using ComputerEquipmentMS;
-using ComputerEquipmentMS.DataAccess;
-using ComputerEquipmentMS.Models.Domain;
-using Mapster;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using ComputerEquipmentMS.ApplicationServiceConfigurators;
 
 SetPointAsNumberDecimalSeparator();
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var builderServices = builder.Services; 
-builderServices.AddControllersWithViews(options => options.Conventions.Add(new AddAuthorizeFiltersControllerConvention()));
-AddAuth();
-AddRepositories();
-AddStoredFunctionsExecutor();
+var services = builder.Services;
+services.AddControllersWithViews(options => options.Conventions.Add(new AddAuthorizeFiltersControllerConvention()));
+
+services.AddConnectionString(builder); 
+services.AddNpgsqlRepositories();
+services.AddNpgsqlStoredFunctionsExecutor();
+
+services.AddAuth();
 
 var app = builder.Build();
 
-RegisterServiceProviderAsStatic();
-RegisterMapsterConfiguration();
+app.RegisterMapsterConfiguration();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -37,7 +35,21 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Customers}/{action=Index}/{id?}/");
+app.MapControllerRoute(
+    name: "sales",
+    pattern: "{controller=Sales}/{action=Index}/{id?}/");
+app.MapControllerRoute(
+    name: "configurations",
+    pattern: "{controller=Configurations}/{action=Index}/{id?}/");
+app.MapControllerRoute(
+    name: "components",
+    pattern: "{controller=Components}/{action=Index}/{id?}/");
+app.MapControllerRoute(
+    name: "auth",
+    pattern: "{controller=Auth}/{action=Login}");
 
 app.Run();
 
@@ -48,67 +60,4 @@ void SetPointAsNumberDecimalSeparator()
     var cultureInfo = (CultureInfo) CultureInfo.CurrentCulture.Clone();
     cultureInfo.NumberFormat.NumberDecimalSeparator = ".";
     CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
-}
-
-void AddAuth()
-{
-    builderServices.AddAuthorization(options => options.AddPolicy("defaultPolicy", policy => policy.RequireAuthenticatedUser()));
-    builderServices
-        .AddAuthentication(options => options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme)
-        .AddCookie(options => options.LoginPath = "/Auth/Login");
-}
-
-void AddRepositories()
-{
-    AddDefaultDbConnectionString();
-    builderServices.AddSingleton<IRepository<Customer, int>, CustomersNpgsqlRepository>();
-    builderServices.AddSingleton<IRepository<Sale, int>, SalesNpgsqlRepository>();
-    builderServices.AddSingleton<IRepository<SalePosition, int>, SalePositionsNpgsqlRepository>();
-    builderServices.AddSingleton<IRepository<ComputerConfiguration, int>, ComputerConfigurationsNpgsqlRepository>();
-    builderServices.AddSingleton<IRepository<Component, int>, ComponentsNpgsqlRepository>();
-    builderServices.AddSingleton<IRepository<ComponentCategory, int>, ComponentCategoriesNpgsqlRepository>();
-    builderServices.AddSingleton<IRepository<ComponentManufacturer, int>, ComponentManufacturersNpgsqlRepository>();
-}
-
-void AddDefaultDbConnectionString()
-{
-    var connectionString = DefaultDbConnectionString();
-    builderServices.AddSingleton(connectionString);
-}
-
-IDbConnectionString DefaultDbConnectionString()
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultDbConnection");
-    if (connectionString is null)
-        throw new NullReferenceException("Default database is not configured for this application");
-
-    return new DbConnectionString(connectionString);
-}
-
-void AddStoredFunctionsExecutor() => 
-    builderServices.AddSingleton<NpgsqlStoredFunctionsExecutor>();
-
-void RegisterServiceProviderAsStatic() => 
-    StaticServiceProvider.Services = app.Services;
-
-void RegisterMapsterConfiguration() =>
-    TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
-
-void MapControllers()
-{
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Customers}/{action=Index}/{id?}/");
-    app.MapControllerRoute(
-        name: "sales",
-        pattern: "{controller=Sales}/{action=Index}/{id?}/");
-    app.MapControllerRoute(
-        name: "configurations",
-        pattern: "{controller=Configurations}/{action=Index}/{id?}/");
-    app.MapControllerRoute(
-        name: "components",
-        pattern: "{controller=Components}/{action=Index}/{id?}/");
-    app.MapControllerRoute(
-        name: "auth",
-        pattern: "{controller=Auth}/{action=Login}");
 }
